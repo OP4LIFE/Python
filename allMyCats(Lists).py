7c7c17ad1027fe754e63f0ec1f9457a1dfd10848
OP4LIFE <matey.vesel@gmail.com>
2021-07-15 10:32:21 +0200

    Update and rename allMyCats.py to allMyCats(Lists).py

---
+++ allMyCats(Lists).py
@@ -0,0 +1,12 @@
+# Ask for the cat's name.
+names = []
+name = 1
+while name != '':
+    print('Enter either the name of the ' + str(len(names)+1) + '. cat or nothing to stop.')
+    name = input()
+    names = names + [name]
+    
+# Print all names. 
+print('The cat names are:')
+for index in names:
+    print(index)
--- allMyCats.py
+++
@@ -1,9 +0,0 @@
-names = []
-name = 1
-while name != '':   # Ask for a name.
-    print('Enter either the name of the ' + str(len(names)+1) + '. cat or nothing to stop.')
-    name = input()
-    names = names + [name]
-print('The cat names are:')     # After stopping, print all names. 
-for index in range(len(names)):
-    print(names[index])
