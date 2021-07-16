ef6811510a6768b623daa2b23118c47d7b58edab
OP4LIFE <matey.vesel@gmail.com>
2021-07-16 11:58:35 +0200

    Update myPets(Lists).py

--- myPets(Lists).py
+++ myPets(Lists).py
@@ -1,13 +1,11 @@
 # A list of pets.
 pets = ['Blacky', 'Bobby']
-
 # Ask for a guess.
 print('Press enter to quit.')
 print('Guess what\'s the name of my pet.')
 pet = 1
 while pet != '':  
-    pet = input()
-    
+    pet = input() 
 # Check the guess.
     if pet in pets:
         print('You guessed!')
