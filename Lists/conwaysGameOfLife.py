6646ce0b9bcf875b797535b409b17fc3dfa12b33
OP4LIFE <matey.vesel@gmail.com>
2021-08-08 20:53:12 +0200

    Add files via upload

--- Lists/conwaysGameOfLife.py
+++ Lists/conwaysGameOfLife.py
@@ -1,39 +1,42 @@
+
+
+# CONWAYS GAME OF LIFE
+# https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
+
 import copy
-# If list's axis are not the same length, the second condition on line 22 will be amiss.
-# Note that the condition on line 38 works only if live states are not labeled with falsey values ('', 0, 0.0).
+
+# Do not change the symbols for live and dead squares as it will interfere with the code.
+# x and y-axis must be the same size otherwise the second condition on line 34 isn't correct.
 
 # Creating a field of squares where each one has its own coorinates and a state. States of the dead squares are represented with zeroes, meanwhile states of the live squares are represented with ones. 
-# Be aware that the state of a field with coordinates (x, y) is obtained from the list with reversed indexes field[y][x].
+# Be aware that the square with coordinates (x, y) is obtained from the list with indexes field[y][x].
 field = [[0, 0, 0, 0, 0, 0],  # y = 0
-         [0, 0, 0, 0, 0, 0],  # y = 1
-         [0, 1, 0, 1, 0, 0],  # y = 2
-         [0, 0, 1, 1, 0, 0],  # y = 3
-         [0, 0, 1, 0, 0, 0],  # y = 4
+         [0, 0, 1, 0, 0, 0],  # y = 1
+         [0, 0, 0, 1, 0, 0],  # y = 2
+         [0, 1, 1, 1, 0, 0],  # y = 3
+         [0, 0, 0, 0, 0, 0],  # y = 4
          [0, 0, 0, 0, 0, 0]]  # y = 5  
 #    x =  0  1  2  3  4  5
 
-endField = [[0, 0, 0, 0, 0, 0],  # y = 0
-            [0, 0, 1, 0, 0, 0],  # y = 1
-            [0, 0, 0, 1, 0, 0],  # y = 2
-            [0, 1, 1, 1, 0, 0],  # y = 3
-            [0, 0, 0, 0, 0, 0],  # y = 4
-            [0, 0, 0, 0, 0, 0]]  # y = 5  
-#       x =  0  1  2  3  4  5
+# Creating a field where new states will be separately written so as to not influence the operating one.
+nextField = copy.deepcopy(field)
 
 
-def image():
-    # The ensuing for loops allow us to read the fields' states.
+# The main function which creates and prints the next generation based on the rules.
+def generation():
+    global field
+
+    # The ensuing for loops allow us to read the squares' states.
     for y in range(len(field)):         # E.g. field[3]
         for x in range(len(field[y])):  # E.g. field[3][2]
-            # Dead and live neighbours' coordinates based on the issued square - the missing field[y][x], which is not counted as a neighbour.
-
+           
             # Checking and fixing if neighbours' indexes are negative or out of list's range. May happen to squares at the edge of the field.
             checkIndexes = [x-1, x+1, y-1, y+1]
             for i in range(len(checkIndexes)):
-                if checkIndexes[i] < 0 or checkIndexes[i] > len(checkIndexes)-1: # The second condition works only when x-axis and y-axis are the same lenght.
+                if checkIndexes[i] < 0 or checkIndexes[i] > len(field)-1:
                     checkIndexes[i] = 0
 
-            # Creating a list of live and dead neighbours.
+            # Creating a list of live and dead neighbours in order to count the live ones.
             left = checkIndexes[0]
             right = checkIndexes[1]
             top = checkIndexes[2]
@@ -48,54 +51,39 @@ def image():
                 if neighbour:
                     liveNeighbours += 1
 
-            #print('(' + str(x) + ', ' + str(y) + '): ' + str(field[y][x]) + ' with ' + str(liveNeighbours) + ' neighbours.')
-            # E.g. '(3, 2): 1 with 3 neighbours.'
-
-
-            # 1. rule
-            if field[y][x]: # works only if zeroes and ones
+            # 1. rule: If a live square has two or three live neighbours, it stays alive, else it dies. 
+            if field[y][x]:
                 if liveNeighbours == 2 or liveNeighbours == 3:
-                    endField[y][x] = field[y][x]
+                    nextField[y][x] = field[y][x]
                 else:
-                    endField[y][x] = 0
+                    nextField[y][x] = 0
               
-            # 2. rule  
+            # 2. rule: If a dead square has exactly three neighbours, it becomes alive, else it stays dead. 
             else:
                 if liveNeighbours == 3:
-                    endField[y][x] = 1
+                    nextField[y][x] = 1
                 else:
-                    endField[y][x] = field[y][x]
+                    nextField[y][x] = field[y][x]
 
+    # Copying the modified field into the original one to provide new values for operation. Note that the list isn't copies just by reference.
+    field = copy.deepcopy(nextField)
+            
+    # Print the generation.
+    for line in nextField:
+        print(line)        
 
-
+# Interface.
 print('Field: ')
 for line in field:
     print(line)
-print()
-
-for i in range(int(input('Enter a number of iterations: '))):
-    image()
-    field = copy.deepcopy(endField)
-    print(str(i + 1) + '. ITERATION')
-    for line in field:
-        print(line)
-    print()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print('\n\n')
+print('Enter any key for next generation or \'q\' to quit.')
+generations = [0]
+for i in generations:
+    if input() != 'q':
+        print(str(i + 1) + '. generation')
+        generation()
+        generations += [generations[i] + 1] 
+        
+   
 
