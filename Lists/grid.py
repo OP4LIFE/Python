97a5dcc697b0858113f23e6d9b62330c1d230ac7
OP4LIFE <matey.vesel@gmail.com>
2021-08-11 12:40:08 +0200

    Update grid.py

--- Lists/grid.py
+++ Lists/grid.py
@@ -8,6 +8,7 @@ grid = [['.', '.', '.', '.', '.', '.'],
         ['.', 'O', 'O', '.', '.', '.'],
         ['.', '.', '.', '.', '.', '.']]
 
+# The following for loop prints the list's items starting with the whole left edge and going right.
 for x in range(len(grid[0])):
     print()
     for y in range(len(grid)):
