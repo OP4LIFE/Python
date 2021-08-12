a0cc0d0f093ab578ae66f914c41b0d6e90a7f0d5
OP4LIFE <matey.vesel@gmail.com>
2021-08-12 14:20:44 +0200

    Update brithdays.py

--- Lists/brithdays.py
+++ Lists/brithdays.py
@@ -1,15 +1,9 @@
-'''
-
-The following program prints the birthday date associated with the entered name.
-If the name doesn't exists, the program will ask you to add one.
-In all cases, you enter 0 or just enter to quit or cancel.
-
-'''
 
 # Creating a dictionary where keys are names and values are dates.
 birthdays = {'Matej': '23. January 2004'}
 
 # Main program loop.
+print('In all cases, enter 0 or just enter to quit or cancel.')
 name = 1 
 while name:
     
