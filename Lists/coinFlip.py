9fb80afe34eaea3d55749b2040f09f649a02dd8f
OP4LIFE <matey.vesel@gmail.com>
2021-08-12 09:51:21 +0200

    Update coinFlip.py

--- Lists/coinFlip.py
+++ Lists/coinFlip.py
@@ -4,21 +4,20 @@ import random
 # check how many 6 same values in row
 # repeat the process 10,000 times. 
 
-sixInRow = 0 
+values = []
+sixInRow = 0
+numOfFlips = 10000
 
-for i in range(10000):
-	
-	# 100 random flips stored
-	flips = []
-	for i in range(100):
-		flips.append(random.randint(0, 1)) 
+
+for i in range(numOfFlips):
+	values.append(random.randint(0, 1)) 
 
 	# checking 6 in a row
-	for i in range(len(flips) - 5):
-		if flips[i] == flips[i + 1] == flips[i + 2] == flips[i + 3] == flips[i + 4] == flips[i + 5]:
-			sixInRow += 1
+for i in range(len(values) - 5):
+	if values[i] == values[i + 1] == values[i + 2] == values[i + 3] == values[i + 4] == values[i + 5]:
+		sixInRow += 1
 	
 # Print the possibility 
-print('Coin flips: 10.000 * 100')
-print('Six same values in row: ' +str(sixInRow))
-print('Possibility ', 100 * sixInRow // 1000000, '%', sep = '')
+print('Coin flips:', numOfFlips)
+print('Six same values in row:', sixInRow)
+print('Possibility: %s%%' % ((numOfFlips / sixInRow) * 100))
