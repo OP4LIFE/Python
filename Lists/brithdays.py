da25c61bb047a0b35b2cc26860c773971b2b7f94
OP4LIFE <matey.vesel@gmail.com>
2021-08-12 14:55:05 +0200

    Update brithdays.py

--- Lists/brithdays.py
+++ Lists/brithdays.py
@@ -1,20 +1,22 @@
+print('BIRTHDAYS\n')
 
 # Creating a dictionary where keys are names and values are dates.
 birthdays = {'Matej': '23. January 2004'}
 
 # Main program loop.
-print('In all cases, enter 0 or just enter to quit or cancel.')
+print('(i) In all cases, enter 0 or just enter to quit or cancel.\n')
 name = 1 
 while name:
     
     # Ask for a name and print the date.
-    name = input('Enter a name: ')
+    name = input('Name: ')
     if name in birthdays:
-        print('Birthday date: ' + birthdays[name])
+        print('Birthday: ' + birthdays[name] + '\n')
     
     # Otherwise, ask to add a date.
     elif name:
         date = input('Add a date: ')
+        print()
         if date:
             birthdays[name] = date
 
