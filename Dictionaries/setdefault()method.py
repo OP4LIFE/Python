4cf23d61e74d9481f65df0dc05e31bbefdb02fee
OP4LIFE <matey.vesel@gmail.com>
2021-08-14 19:42:39 +0200

    Update setdefault()method.py

--- Dictionaries/setdefault()method.py
+++ Dictionaries/setdefault()method.py
@@ -11,7 +11,25 @@ def setdefault(key, value, dictionary):
 
 # Testing
 dictionary = {'key': 'value'}
+
+print('USING THE FUNCTION')
 print('Printing \'value\':', setdefault('key', 'value', dictionary))
-print('Printing \'new value\':', setdefault('Key', 'new value', dictionary))
+print('Printing \'new value\':', setdefault('key', 'new value', dictionary))
+print('Printing the dictionary:', dictionary)
+dictionary = {'key': 'value'}
+
+print('\nUSING THE METHOD')
+print('Printing \'value\':', dictionary.setdefault('key', 'value'))
+print('Printing \'new value\':', dictionary.setdefault('key', 'new value'))
+print('Printing the dictionary:', dictionary)
+
+
+'''
+
+Notice the behaviour where the keys where key passed to the method is the same, meanwhile the value is different.
+In theory, the method should create two identical keys with seperate values.
+What happens is, that the method prints 'New value' which isn't in any key-value pair.
+
+'''
 
 
