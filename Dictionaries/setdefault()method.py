78575f0ef78acbeda384c6b63257913332322cfe
OP4LIFE <matey.vesel@gmail.com>
2021-08-14 18:39:55 +0200

    Update setdefault()method.py

--- Dictionaries/setdefault()method.py
+++ Dictionaries/setdefault()method.py
@@ -1,9 +1,13 @@
 
 # The program simulates the dictionary setdefault() method using a normal function.
 
+# The function
 def getdefault(key, value, dictionary):
     if key in dictionary:
         return dictionary[key]
     else:
-        dictionary[key: value]
+        dictionary[key] = value
         return dictionary[key]
+
+# Testing
+dictionary = {
