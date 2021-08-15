878a00db8fd044caf64f378f2112c333e60ffdb2
OP4LIFE <matey.vesel@gmail.com>
2021-08-15 18:46:40 +0200

    Update characterCounter2.py

--- Dictionaries/characterCounter2.py
+++ Dictionaries/characterCounter2.py
@@ -1,21 +1,23 @@
 
 # The follow program prints the number of characters based on the text entered.
 print('CHARACTER COUNTER 2')
-print('(i) enter enter to exit.')
-
-# Asks for text.
-text = input('\nEnter text: ')
-
-# Main loop.
-characters = {}
-for character in text:
-    characters.setdefault(character, 0)
-    character[character] += 1   
-
-# Printing number of individual characters.
-print('Characters: ')
-for item in characters.items():
-    print(item)
-
+print('The program is case-sensitive.')
+print('(i) Enter enter to exit.')
+text = 1
+while text:
+	
+	# Asks for text.
+	text = input('\nEnter text: ')
+	
+	# Main loop.
+	characters = {}
+	for character in text:
+	    characters.setdefault(character, 0)
+	    characters[character] += 1   
+	
+	# Printing number of individual characters.
+	print('Characters: ')
+	for item in characters.items():
+	    print(item)
 
 
