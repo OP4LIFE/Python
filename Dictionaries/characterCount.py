a775350235952d146e6bb9282a33299efb773ccc
OP4LIFE <matey.vesel@gmail.com>
2021-08-15 12:39:16 +0200

    Create characterCount.py

---
+++ Dictionaries/characterCount.py
@@ -0,0 +1,30 @@
+
+# The following program asks for a word or a sentance, stores the occurrences of each character into a dictionary and prints the dictionary.
+print('CHARACTER COUNTER')
+print('The counter is case-sensitive.')
+print('(i) Enter enter to exit.')
+string = 1
+while string:
+	
+	# Ask for a string.
+	string = input('\nEnter some text: ')
+	
+	# A dictionary for storing 	occurrences.
+	occurrences = {}
+	
+	# A loop for storing occurances.
+	for i in range(len(string)):
+	
+	    # If the character is already in occurrences dictionary, add one,    
+	    if string[i] in occurrences.keys():
+	        occurrences[string[i]] += 1
+	    # else, create a key for the character and set its value to one for the first occurrence.
+	    else:
+	        occurrences [string[i]] = 1
+	
+	# Printing occurrences
+	print('Occurrences: ')
+	for item in occurrences.items():
+	    print(item)
+
+
