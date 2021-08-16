3128d65940ee490a375c13fb1de8ed6676f84378
OP4LIFE <matey.vesel@gmail.com>
2021-08-16 12:48:40 +0200

    Create ticTacToeGame.py

---
+++ Dictionaries/ticTacToeGame.py
@@ -0,0 +1,5 @@
+
+# The code is for playing Tic-Tac-Toe
+print('TIC-TAC-TOE GAME \n')
+print('(c) player vs computer \n (p) player vs player \n (cc) computer vs computer \n')
+print('Which player do you want to be?')
