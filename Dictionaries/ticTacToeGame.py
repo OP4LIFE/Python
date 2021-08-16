5577938d8f72da0fae5cbc8c13733d7e332eec61
OP4LIFE <matey.vesel@gmail.com>
2021-08-16 19:09:19 +0200

    Update ticTacToeGame.py

--- Dictionaries/ticTacToeGame.py
+++ Dictionaries/ticTacToeGame.py
@@ -3,3 +3,6 @@
 print('TIC-TAC-TOE GAME \n')
 print('(c) player vs computer \n (p) player vs player \n (cc) computer vs computer \n')
 print('Which player do you want to be?')
+
+for i in range(8):
+    board[input
