df9d46e4d0ea570fa9e11a8f6b64e08330cf17b8
OP4LIFE <matey.vesel@gmail.com>
2021-08-17 16:07:59 +0200

    Update ticTacToeGame.py

--- Dictionaries/ticTacToeGame.py
+++ Dictionaries/ticTacToeGame.py
@@ -1,9 +1,8 @@
 
 # The code is for playing Tic-Tac-Toe
 print('TIC-TAC-TOE GAME \n')
-print('(c) player vs computer \n (p) player vs player \n (cc) computer vs computer \n')
-print('Which player do you want to be?')
 
+# A dictionary which
 board = {1: ' ', 2: ' ', 3: ' ',
          4: ' ', 5: ' ', 6: ' ',
          7: ' ', 8: ' ', 9: ' '}
@@ -14,13 +13,13 @@ def conditions():
                   [1, 4, 7], [2, 5, 6], [3, 6, 9], # Vertical
                   [1, 5, 9], [3, 5, 7]]            # Diagonal
     for condition in conditions:
-        if board[condition[0]] == board[condition[1]] == board[condition[2]]:
+        if board[condition[0]] == board[condition[1]] == board[condition[2]] == ('X' or 'O'):
             return 1
 
 for i in range(8):
 
     # Player X's turn
-    board[input('Player X: ')] = 'X'
+    board[int(input('Player X: '))] = 'X'
     print(board)   
     if conditions():
         print('Player X is the winner')
@@ -30,7 +29,7 @@ for i in range(8):
         break
     
     # Player O's turn
-    board[input('Player O')] = 'O'
+    board[int(input('Player O'))] = 'O'
     print(board) 
     if conditions():
         print('Player O is the winner')
