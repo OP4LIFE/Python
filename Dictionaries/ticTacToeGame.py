ac4c0484294af9abbe865a26514d6571a32a3856
OP4LIFE <matey.vesel@gmail.com>
2021-08-18 22:03:24 +0200

    Update ticTacToeGame.py

--- Dictionaries/ticTacToeGame.py
+++ Dictionaries/ticTacToeGame.py
@@ -1,15 +1,22 @@
+
+# TIC-TAC-TOE GAME
+print('\nTIC-TAC-TOE GAME')
+print('Enter a number to draw the symbol on.\n')
+
+# Modules.
 import random
 
-# The code is for playing Tic-Tac-Toe
-print('TIC-TAC-TOE GAME \n')
-print('(i) Enter \'q\' to exit.')
+# Score counter.
+score = {'X': 0, 'O': 0}
 
+# A dictionary for storing moves.
 board = {}
-while 'q' not in board.values():
-    # A dictionary for storing moves.
-    board = {1: '_', 2: '_', 3: '_',
-             4: '_', 5: '_', 6: '_',
-             7: '_', 8: '_', 9: '_'}
+
+# Main loop.
+while 1:
+    board = {1: 1, 2: 2, 3: 3,
+             4: 4, 5: 5, 6: 6,
+             7: 7, 8: 8, 9: 9}
     
     # Function for cheking winning conditions.
     def conditions():
@@ -17,29 +24,38 @@ while 'q' not in board.values():
                       [1, 4, 7], [2, 5, 6], [3, 6, 9], # Vertical
                       [1, 5, 9], [3, 5, 7]]            # Diagonal
         for condition in conditions:
+            
+            # If three in a row, announce it!
             if board[condition[0]] == board[condition[1]] == board[condition[2]] == ('X' or 'O'):
-                return 1
-    
-    # A loop for maximum of 9 moves for both players.
-    # The starting player has 5 moves while the other has 4.
+                return True
+ 
+    # Game loop.
     player = 'X'
     for move in range(9):
-        
-        # Move stored in board dictipnary.
-        board[int(input('Player ' + player + ': '))] = player
-        
+       
         # Printing the board.
         for key in range(1, 10):
-            print(board[key], end = '')
+            print(board[key], end = ' ')
             if key / 3 == int(key / 3):
                 print()
         
+        # Store the approproate symbol at the inputed number in the board dictionary.
+        try:
+            board[int(input('\nPlayer ' + player + ': '))] = player
+        except:
+            board[int(input('\nPlayer ' + player + ': '))] = player
+        
         #  Checking for three in row.  
         if conditions():
-            print('Player ' + player + ' is the winner')
+            score[player] += 1
+            print('\n--------------------- \n' + \
+                  'Player ' + player + ' is the winner \n\n' + \
+                  'Player X wins: ' + str(score['X']) + '\n' + \
+                  'Player O wins: ' + str(score['O']) + '\n' + \
+                  '---------------------\n')
             break
         
-        # Change players at the end of the move.
+        # Change players at the end of one's turn.
         if player == 'X':
         	player = 'O'
         else:
@@ -47,7 +63,7 @@ while 'q' not in board.values():
        
        # Last move.
         if move == 8:
-            print('It\'s a tie')
+            print('It\'s a tie\n')
             break
         	   
     
