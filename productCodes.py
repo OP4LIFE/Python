ac694589fb03721ca43f988f06d59ed474684d8c
OP4LIFE <matey.vesel@gmail.com>
2021-08-25 16:15:52 +0200

    Update productCodes.py

--- productCodes.py
+++ productCodes.py
@@ -1,14 +1,13 @@
 
-import xlrd
+import xlrd, webbrowser
 
-# Open a workbxook.
-workbook = xlrd.open_workbook("Path to the file")
+# Open a workbook.
+workbook = xlrd.open_workbook(r'C:\Users\matey\Desktop\productCodes.xls')
 sheet = workbook.sheet_by_index(0)
 
-# Read first column manually.
-print(sheet.cell_value(0, 0))
-print(sheet.cell_value(0, 1))
-print(sheet.cell_value(0, 2))
-print(sheet.cell_value(0, 3))
-
-
+# Read the first column.
+for row in range(sheet.nrows):
+    code = int(sheet.cell_value(row, 0)
+    link = 'https://www.qwant.com/?client=brz-brave&q=' + str(code) + '&t=web'
+    webbrowser.open(link)
+# Search for the code on a site.
