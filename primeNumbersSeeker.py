b3caeb763b9f20d599b9d5b3ee58b922ed447a36
OP4LIFE <matey.vesel@gmail.com>
2021-10-26 17:38:43 +0200

    Update primeNumbersSeeker.py

--- primeNumbersSeeker.py
+++ primeNumbersSeeker.py
@@ -4,13 +4,11 @@ numbers = [2, 3, 4]
 prime_numbers = [2, 3]
 
 while 1:
-    try:
-        
+    try: 
         # Add numbers to the numbers list.
         numbers += [numbers[-1] + 1]
 
-        for i in range(1, len(numbers) - 1):
-            
+        for i in range(1, len(numbers) - 1):           
             # Check whether the current number is dividable by previous numbers.
             if numbers[-1] / numbers[i] == int(numbers[-1] / numbers[i]):
                 break
@@ -19,8 +17,7 @@ while 1:
             elif i == len(numbers) - 2:
                 prime_numbers += [numbers[-1]]
 
-    except KeyboardInterrupt:
-        
+    except KeyboardInterrupt:       
         # Write prime numbers to a text file when CTRL-C is pressed.
         with open('Prime numbers.txt', 'w') as output_file:
             output_file.write(f'PYTHON OVERWRITES THIS FILE EACH TIME.\n\nThere are {len(prime_numbers)} prime numbers till number {numbers[-1]}:')
