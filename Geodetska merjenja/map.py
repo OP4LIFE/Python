e093fe2ca641a80caa8d99e710de505ad548ca31
OP4LIFE <matey.vesel@gmail.com>
2022-01-30 14:45:41 +0100

    Update map.py

--- Geodetska merjenja/map.py
+++ Geodetska merjenja/map.py
@@ -1,3 +1,10 @@
+#------------ README ----------------------------
+'''
+Install required libraries from the Command prompt or Terminal.
+Windows: py -m pip install openpyxl matplotlib numpy
+Unix/macOS: python -m pip install openpyxl matplotlib numpy
+'''
+
 import openpyxl
 import matplotlib.pyplot as plt
 import numpy as np
@@ -8,15 +15,15 @@ wb = openpyxl.load_workbook(file)
 
 
 #------------ READ ----------------------------
-sh1 = wb['Sheet1']
+sheet1 = wb['Sheet1']
 
 xpoints = []
 ypoints = []
-for i in range(1, sh1.max_row + 1):
+for i in range(1, sheet1.max_row + 1):
     
     # Read values at current row.
-    x = sh1.cell(column=1, row=i).value
-    y = sh1.cell(column=2, row=i).value
+    x = sheet1.cell(column=1, row=i).value
+    y = sheet1.cell(column=2, row=i).value
     
     # Apply the formula.
     x += 1
@@ -31,12 +38,12 @@ for i in range(1, sh1.max_row + 1):
 # Create Sheet2.
 if 'Sheet2' not in wb.sheetnames:
     wb.create_sheet('Sheet2')
-sh2 = wb['Sheet2']
+sheet2 = wb['Sheet2']
 
 # Write to Sheet2.
-for i in range(1, sh1.max_row + 1):
-    sh2.cell(column=1, row=i).value = xpoints[i - 1]
-    sh2.cell(column=2, row=i).value = ypoints[i - 1]
+for i in range(1, sheet1.max_row + 1):
+    sheet2.cell(column=1, row=i).value = xpoints[i - 1]
+    sheet2.cell(column=2, row=i).value = ypoints[i - 1]
 
 wb.save(file)
 
