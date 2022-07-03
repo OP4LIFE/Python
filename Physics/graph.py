d54fdeed2317ef1123866f6f1047261500db01eb
OP4LIFE <matey.vesel@gmail.com>
2022-07-03 04:40:51 +0200

    Add files via upload

--- Physics/graph.py
+++ Physics/graph.py
@@ -1,6 +1,5 @@
 #------------ README ----------------------------
 '''
-The program draws a map from given coordinates.
 
 Install required libraries:
 Windows: py -m pip install openpyxl matplotlib numpy
@@ -10,30 +9,27 @@ Unix/macOS: python -m pip install openpyxl matplotlib numpy
 import openpyxl
 import matplotlib.pyplot as plt
 import numpy as np
+from math import log10, floor
 
 # Load a file.
-file = 'koordinati.xlsx'
+file = 'graph.xlsx'
 wb = openpyxl.load_workbook(file)
 
 
 #------------ READ ----------------------------
 sheet1 = wb['Sheet1']
 
-xpoints = []
-ypoints = []
+x = []
+y = []
 for i in range(1, sheet1.max_row + 1):
     
     # Read values at current row.
-    x = sheet1.cell(column=1, row=i).value
-    y = sheet1.cell(column=2, row=i).value
-    
-    # Apply the formula.
-#    x += 1
-#    y += 1
+    xpoints = sheet1.cell(column=1, row=i).value
+    ypoints = sheet1.cell(column=2, row=i).value
     
     # Append points to the lists.
-    xpoints.append(x)
-    ypoints.append(y)
+    x.append(xpoints)
+    y.append(ypoints)
 
 #------------ WRITE ----------------------------
 # Create Sheet2.
@@ -43,64 +39,146 @@ sheet2 = wb['Sheet2']
 
 # Write to Sheet2.
 for i in range(1, sheet1.max_row + 1):
-    sheet2.cell(column=1, row=i).value = xpoints[i - 1]
-    sheet2.cell(column=2, row=i).value = ypoints[i - 1]
+    sheet2.cell(column=1, row=i).value = x[i - 1]
+    sheet2.cell(column=2, row=i).value = y[i - 1]
 
 wb.save(file)
 
 
 #------------ DRAW ----------------------------
 
-# Convert lists to arrays.
-xpoints = np.array(xpoints)
-ypoints = np.array(ypoints)
-
-# Similar curve
-[1/4, 1/3, 1/2, 1, 2, 3, 4]
-[16, 9, 4, 1, 1/4, 1/9, 1/16]
-
-xcurve = 
-
-# Axes' settings.
-fig, ax = plt.subplots()
-
-# Simetrical exes
-#ax.set_aspect('equal')
-
-ax.spines['right'].set_visible(True)
-ax.spines['left'].set_visible(True)
-ax.spines['top'].set_visible(True)
-ax.spines['bottom'].set_visible(True)
+def round1(value):
+    return round(value, -int(floor(log10(abs(value)))))
 
 
+def roundPartial (value, resolution):
+    if value > 0 and value < 1:
+         decimal_places = len(str(value).split(".")[1])
+         resolution = resolution * 10 ** -decimal_places
+         
+    return round (value /float(resolution)) * resolution
+    
+    
 
-# Ticks
+# Each point, calculates distance to line, returns average
+def average_distance(slope):
+    distances = []
+    for i in range(len(x)):
+        x_ = x[i]
+        y_ = y[i]
+        
+        d = abs(slope * x_ - y_) / (1 + slope ** 2) ** (1/2)       
+        distances += [d]
+               
+    return sum(distances) / len(distances)  
+
+
+
+def slope():
+    k = [0.0000001, 1, 100000]
+    for o in range(50):
+          
+    # Calculate averages.
+        a = []  
+        for p in range(len(k)):       
+            a += [average_distance(k[p])]
+        
+    # Numerical searching for optimal curve.
+        m = a.index(min(a))
+        if m == 0:
+            k = [k[m], (k[m] + k[m + 1])/2, k[m + 1]]
+            
+        elif m == len(a) - 1:
+            k = [k[m - 1], (k[m - 1] + k[m])/2, k[m]]
+            
+        else:   
+            k = [k[m - 1],    (k[m - 1] + k[m])/2,    k[m],    (k[m] + k[m + 1])/2,    k[m + 1]]
+    #            previous            avarage         current       avarage               next    
+        
+        if o == 49:
+            return k[a.index(min(a))]
+  
+  
 padding = 1.07
-ax.set_xticks(np.arange(0, max(xpoints) * padding, 5))
-ax.set_xticks(np.arange(0, max(xpoints) * padding, 5/5), minor=True)
-ax.set_yticks(np.arange(0, max(ypoints) * padding, 200))
-ax.set_yticks(np.arange(0, max(ypoints) * padding, 200/5), minor=True)
+fig, ax = plt.subplots(2, 1)
 
-# Enables (0, 0) to be on the axes' intersection.
-plt.xlim(0, None)
-plt.ylim(0, None)
+############## PLOT 1 #################    
+x = np.array(x)
+y = np.array(y)
 
-# Grid
-ax.grid(which='both')
+x2 = np.linspace(0, max(x) * padding, 1000)
+y2 = 16704 * x2 ** -2
 
-# Grid transperency
-ax.grid(which='minor', alpha=0.2)
-ax.grid(which='major', alpha=0.5)
+xspacing = round1(max(x) / 8)
+yspacing = round1(max(y) / 6)
+xspacing = roundPartial(xspacing, 5)
+ax[0].set_xticks(np.arange(0, max(x) * padding, xspacing))
+ax[0].set_xticks(np.arange(0, max(x) * padding, xspacing / 5), minor=True)
+ax[0].set_yticks(np.arange(0, max(y) * padding, yspacing))
+ax[0].set_yticks(np.arange(0, max(y) * padding, yspacing / 5), minor=True)
+ax[0].spines['right'].set_color('#D7D7D7')
+ax[0].spines['left'].set_visible(True)
+ax[0].spines['top'].set_color('#D7D7D7')
+ax[0].spines['bottom'].set_visible(True)
+ax[0].grid(which='both')
+ax[0].grid(which='minor', alpha=0.2)
+ax[0].grid(which='major', alpha=0.5)
 
-# Title and lables
-plt.title("o(r)")
-plt.xlabel('r[cm]', loc = "right")
-plt.ylabel('o[lux]', loc = "top", rotation = 0)
 
-# Draw and save the map.
-plt.plot(xpoints, ypoints, '.')
+plt.subplot(2, 1, 1)
+plt.plot(x, y, 'xk')
+plt.plot(x2, y2, '-')
 
-#plt.savefig('Zemljevid.svg')
+plt.xlim(0, max(x) * padding)
+plt.ylim(0, max(y) * padding)
+plt.tight_layout(pad=3)
+plt.grid('both', 'both')
+plt.title(label = r'$\bf{o(r)}$', pad = '20.0')
+plt.xlabel(r'$r[cm]$', loc = 'right')
+plt.ylabel('o[lux]', loc = 'top', rotation = 0) 
+plt.legend(['Meritve', r'$y = k \cdot r^{-2}$'], loc = 'upper right')
+
+
+############# PLOT 2 (linearizacija) #######
+x = list(x)
+for i in range(len(x)):
+    x[i] = float(x[i]) ** -2
+x = np.array(x)
+
+
+x2 = np.linspace(0, max(x) * padding, 1000)
+y2 = slope() * x2
+
+xspacing = round1(max(x) / 8)
+yspacing = round1(max(y) / 6)
+xspacing = roundPartial(xspacing, 5)
+ax[1].set_xticks(np.arange(0, max(x) * padding, xspacing))
+ax[1].set_xticks(np.arange(0, max(x) * padding, xspacing / 5), minor=True)
+ax[1].set_yticks(np.arange(0, max(y) * padding, yspacing))
+ax[1].set_yticks(np.arange(0, max(y) * padding, yspacing / 5), minor=True)
+ax[1].spines['right'].set_color('#D7D7D7')
+ax[1].spines['left'].set_visible(True)
+ax[1].spines['top'].set_color('#D7D7D7')
+ax[1].spines['bottom'].set_visible(True)
+ax[1].grid(which='both')
+ax[1].grid(which='minor', alpha=0.2)
+ax[1].grid(which='major', alpha=0.5)
+
+
+plt.subplot(2, 1, 2)
+plt.plot(x, y, 'xk')
+plt.plot(x2, y2, '-')
+
+plt.xlim(0, max(x) * padding)
+plt.ylim(0, max(y) * padding)
+plt.tight_layout(pad=3)
+plt.grid('both', 'both')
+plt.title(label = r'$\bf{o(r^{-2})}$', pad = '20.0')
+plt.xlabel(r'$r^{-2}[cm]$', loc = 'right')
+plt.ylabel('o[lux]', loc = 'top', rotation = 0) 
+plt.legend(['Meritve', r'$y = k \cdot r$'], loc ='center right')
+
+#plt.savefig("graf.png", dpi = 600, orientation = 'portrait', papertype = 'a4', format = 'png', bbox_inches = 'tight', pad_inches = 1, transparent = None)
+#plt.savefig("graf.svg", dpi = 600, orientation = 'portrait', papertype = 'a4', format = 'svg', bbox_inches = 'tight', pad_inches = 1, transparent = None)
 plt.show()
-
 
